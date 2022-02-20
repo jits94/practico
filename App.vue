a9626e7449d78432a07eea06859aeb0084072fd2
Jorge Moron <moronjorge94@gmail.com>
2022-02-20 17:43:51 -0400

    primera subida

---
+++ App.vue
@@ -0,0 +1,50 @@
+<template>
+  <view class="container">
+    <text class="titulo">2DO EJERCICIO</text>
+    <view class="imagen">
+    <image class="imagenes"
+          :style="{width: 300, height: 300,}"
+          :source="{uri: 'https://emprendecontuweb.com/wp-content/uploads/2018/07/web-development-1140x569.jpg'}"
+    />
+     </view>
+     <view class="contenido">
+      <text class="detalle">DETALLE:</text>
+      <text class="parrafo">Este es el segundo modelo que solicito el ingeniero que pudieramos realizar, este modelo se ha realizado con puros stilos css, por lo tanto no hemos ocupado el framework native base, este framework sera ocupado para los otros ejemplos que se realizaran en esta tarea</text>
+    </view>
+  </view>
+</template>
+
+<style>
+.contenido{
+  margin-left: 20px;
+  margin-right: 20px;
+}
+.parrafo{
+  padding-top: 15px;
+  font-size: 15px;
+}
+.detalle{
+  align-content: flex-start;
+  font-size: 20px;
+}
+.titulo{
+  font-size: 30px;
+  padding-bottom: 15px;
+}
+.imagen{
+  align-items: center;
+  padding-bottom: 20px;
+}
+.imagenes{
+  border-radius: 300;
+}
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.text-color-primary {
+  color: blue;
+}
+</style>
---
+++ ejercicios/ejemplo3.vue
@@ -0,0 +1,25 @@
+<template>
+  
+  <native-base-provider>
+
+     <box p="30px" bg="#3aba1c" h="100px"> </box>
+     <box p="30px" bg="#2299a1" h="200px"> </box>
+     <box p="30px" bg="#4198cc" h="50px"> </box>
+     <box p="30px" bg="#32abcd" h="250px"> </box>
+    
+        
+  
+  </native-base-provider>
+ 
+</template>
+
+<script>
+  import { NativeBaseProvider,Button, Box,Container, Text,Heading, Stack, HStack, Center} from "native-base";
+
+  export default{
+    data:{
+
+    },
+    components:{NativeBaseProvider,Button, Box, Container, Text, Heading, Stack, HStack, Center}
+  }
+</script>
