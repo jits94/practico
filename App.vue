abc2dc9d18de21d3f6c7e5431896f7907d99f782
Jorge Moron <moronjorge94@gmail.com>
2022-02-21 16:23:13 -0400

    matrices

--- App.vue
+++ App.vue
@@ -1,36 +1,36 @@
 <template>
-  <view class="container">
-    <view class="imagen">
-    <image
-          :style="{width: 300, height: 300,}"
-          :source="{uri: 'https://emprendecontuweb.com/wp-content/uploads/2018/07/web-development-1140x569.jpg'}"
-    />
-     </view>
-     <view class="contenido">
-      <button title="Learn More" color="#841584" />
-       
-    </view>
-    <view>
-      <button class="boton" title="Learn More" color="#841584" />
-    </view>
-  </view>
+  <native-base-provider>
+    <scrollView>
+      <box p="40px">
+        <text>Filas</text>
+        <input type="text"/>
+        <text>Columnas</text>
+        <input type="text"/>
+        <box pt="30px">
+          <button P="10px" size="sm">GENERAR</button> 
+        </box>
+      </box>
+      <box maxWidth="100%" alignItems="center" bg="#3aba1c"> 
+          <vStack space="2.5" mt="4" px="8">     
+              <box v-for="i in filas" v-bind:key="i.id">
+                <stack direction="row" > 
+                  <box v-for="j in columnas" v-bind:key="j.id"> 
+                    <input type="text" width="50px" h="50px" m="1" defaultValue="0"/>
+                  </box> 
+                </stack>         
+              </box>   
+          <vStack>
+        </box>
+      <scrollView>
+  </native-base-provider>
 </template>
-
-<style>
-.boton{
- 
-}
-.contenido{
- padding-bottom: 10px;
-}
-.imagen{
-  align-items: center;
-  padding-bottom: 20px;
-  padding-top: 20px;
-}
-.container {
-  background-color: white;
-  align-items: center;
-  justify-content: center;
-}
-</style>
+<script>
+  import { NativeBaseProvider,Button, Box,Container, Text,Heading, Stack, HStack, Center, Input, ScrollView, VStack} from "native-base";
+  export default{
+    data:{
+    filas: 4,
+    columnas: 3,
+    },
+    components:{NativeBaseProvider,Button, Box, Container, Text, Heading, Stack, HStack, Center,Input,ScrollView, VStack}
+  }
+</script>
---
+++ ejercicios/ejercicio1.vue
@@ -0,0 +1,36 @@
+<template>
+  <view class="container">
+    <view class="imagen">
+    <image
+          :style="{width: 300, height: 300,}"
+          :source="{uri: 'https://emprendecontuweb.com/wp-content/uploads/2018/07/web-development-1140x569.jpg'}"
+    />
+     </view>
+     <view class="contenido">
+      <button title="Learn More" color="#841584" />
+       
+    </view>
+    <view>
+      <button class="boton" title="Learn More" color="#841584" />
+    </view>
+  </view>
+</template>
+
+<style>
+.boton{
+ 
+}
+.contenido{
+ padding-bottom: 10px;
+}
+.imagen{
+  align-items: center;
+  padding-bottom: 20px;
+  padding-top: 20px;
+}
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+}
+</style>
